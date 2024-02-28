2b44991caf258054b17face99a5a055bd71ec1bb
shashank-1986 <miracle85.shashank@gmail.com>
2024-02-29 00:51:52 +0530

    changes

---
+++ kafka-consumer-database/src/main/java/net/javaguides/kafkaconsumerdatabase/KafkaConsumerDatabaseApplication.java
@@ -0,0 +1,11 @@
+package net.javaguides.kafkaconsumerdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDatabaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerDatabaseApplication.class, args);
+    }
+}
---
+++ kafka-consumer-database/src/test/java/net/javaguides/kafkaconsumerdatabase/KafkaProducerDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.kafkaconsumerdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
